39b0d5a2e65b4248cd52616c4564fe2c5797749a
jabneyhastings <jabneyhastings@gmail.com>
2025-09-12 11:44:19 +0100

    fix image paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vasari-astro' : '/',
+});
